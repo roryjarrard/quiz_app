7a0224a27ce68d36bdca3a85f42a9d4da6003c10
Rory Jarrard <rjarrard@gmail.com>
2024-05-10 14:19:26 -0600

    iterate into widgets

--- lib/pages/questions.dart
+++ lib/pages/questions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,6 +10,8 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -16,14 +19,14 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton('Answer 1', () {}),
-          AnswerButton('Answer 2', () {}),
-          AnswerButton('Answer 3', () {}),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(item, () {});
+          }),
         ],
       ),
     );
