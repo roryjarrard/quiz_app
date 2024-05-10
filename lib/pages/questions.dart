712d2502c4560ac6fd45b5a535c0fab38aac67f7
Rory Jarrard <rjarrard@gmail.com>
2024-05-10 14:23:12 -0600

    styling

--- lib/pages/questions.dart
+++ lib/pages/questions.dart
@@ -16,18 +16,23 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((item) {
-            return AnswerButton(item, () {});
-          }),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((item) {
+              return AnswerButton(item, () {});
+            }),
+          ],
+        ),
       ),
     );
   }
