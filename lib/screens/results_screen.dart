6b11eb3a960ae360dfde856b17f389a450cdcc3b
Rory Jarrard <rjarrard@gmail.com>
2024-05-15 14:01:08 -0600

    use getter

--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -12,7 +12,7 @@ class ResultsScreen extends StatelessWidget {
   final List<String> chosenAnswers;
   final void Function() onRestart;
 
-  List<Map<String, Object>> getSummaryData() {
+  List<Map<String, Object>> get summaryData {
     final List<Map<String, Object>> summaryData = [];
 
     for (var i = 0; i < chosenAnswers.length; i++) {
@@ -29,7 +29,6 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final summaryData = getSummaryData();
     final numTotalQuestions = questions.length;
     final numCorrectQuestions = summaryData.where(
       (item) {
